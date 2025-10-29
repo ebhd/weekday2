f152cb087415e95d97e831e329997c8d2a00a508
NotCure <bruxill456@gmail.com>
2025-10-29 22:33:03 +0100

    added boilerplate

---
+++ config/routes.ts
@@ -0,0 +1,8 @@
+export const routes = {
+    home: "/",
+    rankings: "/rankings",
+    submit:"/dashboard/song?new=1",
+    practice: "/dashboard/practice",
+    dashboard: "/dashboard",
+    settings: "/dashboard/settings",
+}
---
+++ config/seo.ts
@@ -0,0 +1,4 @@
+export const defaultSEO = {
+  title: "Drillrecord",
+  description: "A app to record and track your drilling practice sessions.",
+};
