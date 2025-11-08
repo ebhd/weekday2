879de536baceaeff71c77d49e35476915c5bdd3a
NotCure <bruxill456@gmail.com>
2025-11-08 18:13:30 +0100

    added things

--- components/blocks/BackgroundFx.tsx
+++ components/blocks/BackgroundFx.tsx
@@ -4,11 +4,13 @@ export function BackgroundFx() {
   return (
     <div
       aria-hidden
-      className="pointer-events-none fixed inset-0 -z-10 overflow-hidden"
+      className="pointer-events-none fixed inset-0 z-0 overflow-hidden"
     >
+      {/* Green radial gradient */}
       <div className="absolute inset-0 bg-[radial-gradient(3300px_1100px_at_50%_-10%,_rgba(64,255,80,0.25),_transparent_35%)] opacity-20" />
 
-      <div className="absolute inset-0 opacity-[0.6%]">
+      {/* Background image */}
+      <div className="absolute inset-0 opacity-60">
         <Image
           src="/backgroundHero.png"
           alt=""
@@ -18,8 +20,11 @@ export function BackgroundFx() {
         />
       </div>
 
-      {/* <div className="absolute inset-0 opacity-[.07] mix-blend-overlay"
-           style={{ backgroundImage: 'url(/images/noise-512.png)' }} /> */}
+      {/* Optional noise texture */}
+      {/* <div
+        className="absolute inset-0 opacity-10 mix-blend-overlay"
+        style={{ backgroundImage: 'url(/images/noise-512.png)' }}
+      /> */}
     </div>
   );
 }
