15f3cec082dec067c0852876f103c0111fc4f419
NotCure <bruxill456@gmail.com>
2025-11-09 23:28:30 +0100

    added testbar somhow fixed the rankding table

--- components/blocks/RankingHomeSection.tsx
+++ components/blocks/RankingHomeSection.tsx
@@ -5,10 +5,12 @@ export function RankingHomeSection() {
     <div className="justify-center flex flex-col items-center gap-4">
       <TestBar
         text="Explore top rated and viewed artists on our Platform And Worldwide"
-        width={32}
         icon={true}
       />
       <RankingTable />
+      <button className="w-full h-10 border border-muted-fg rounded-xl flex items-center justify-center font-sans">
+        <p>Load more</p>
+      </button>
     </div>
   );
 }
