d2337ef2fcbad77b5653af813d7f72e4e77c33ca
NotCure <bruxill456@gmail.com>
2025-11-28 20:36:04 +0100

    copy

--- features/ranking/components/RankingTableParts/RankingRow.tsx
+++ features/ranking/components/RankingTableParts/RankingRow.tsx
@@ -16,25 +16,25 @@ export function RankingRow(props: Props) {
     <div
       className={`
         ${rankingDesktopGrid} ${rankingMobileGrid} px-4 py-2
-        gap-y-3 lg:gap-y-0
+        gap-y-3 md:gap-y-0
         border-b border-white/10
       `}
     >
-      <div className="text-center font-display text-4xl lg:text-5xl">
+      <div className="text-center font-display text-4xl md:text-5xl">
         {props.rank}.
       </div>
 
-      <div className="flex items-center gap-3 justify-start w-full lg:pl-20 min-w-0">
+      <div className="flex items-center gap-3 justify-start w-full md:pl-20 min-w-0">
         <Image
           src={props.artistImageUrl || "/default-avatar.png"}
           alt={props.artistName}
           width={64}
           height={64}
-          className="rounded-full object-cover shrink-0 w-14 h-14 lg:w-16 lg:h-16 lg:block hidden"
+          className="rounded-full object-cover shrink-0 w-14 h-14 md:w-16 md:h-16 md:block hidden"
         />
         <div className="flex flex-col min-w-0">
           <Link href={`/songs/${props.songSlug}`}>
-            <span className="text-base truncate lg:text-lg hover:underline">
+            <span className="text-base truncate md:text-md hover:underline">
               {props.songTitle}
             </span>
           </Link>
@@ -46,7 +46,7 @@ export function RankingRow(props: Props) {
         </div>
       </div>
 
-      <div className="w-full max-w-full justify-center lg:max-w-[380px]">
+      <div className="w-full max-w-full min-w-0 justify-center lg:max-w-[380px]">
         <SongPlayer
           url={props.audioUrl}
           onPlayProgress={
