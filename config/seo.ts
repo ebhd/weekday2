c2c157f453eaad1d87420ef9421faaf026e3469a
NotCure <bruxill456@gmail.com>
2025-12-05 00:02:29 +0100

    added blogs

--- config/seo.ts
+++ config/seo.ts
@@ -1,4 +1,4 @@
 export const defaultSEO = {
   title: "Weekday",
-  description: "A app to record and track your drilling practice sessions.",
+  description: "A app to record and promote your underground artworks.",
 };
